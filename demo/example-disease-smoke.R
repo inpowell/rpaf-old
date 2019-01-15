4bc5bec7590fa353a691f544b2cea1f0e792fe10
Ian Powell <i.powell@student.unsw.edu.au>
2019-01-15 15:28:00 +1100

    Remove old disease PAF files that have been superseded

--- demo/example-disease-smoke.R
+++ demo/example-disease-smoke.R
@@ -10,35 +10,37 @@ library(rpaf)
 
 # prepare the data, similar in function to the SAS gen_data macro, but
 # specialised for disease PAFs
-smoke_data <- dpaf_data(
-  rawdata = minifhs,
-  variables = c("B_COHORT", "SEX", "SMOKE"),
-  time_breaks = c(0,5,10,15,17), # could use, e.g. `c(seq(0,17,5), 17)`
+smoke_data <- gen_data(
+  indata = minifhs,
   id_var = "ID",
-  death_time = "DEATH_FT",
+  ft_breaks = c(0,5,10,15,17),
   death_ind = "DEATH",
-  disease_time = "DIAB_FT",
+  death_time = "DEATH_FT",
   disease_ind = "DIAB",
+  disease_time = "DIAB_FT",
+  variables = c("B_COHORT", "SEX", "SMOKE"),
   period_factor = "F_PERIOD",
-  ft_end = "F_TIME"
+  time_var = "F_TIME"
 )
 
-# Fit the survival regression models, and return the information obatined. This
-# is similar in function to the first part of the est_matrix macro, but doesn't
-# calculate relative risks. Instead, it only fits the survival regression models
-# for both disease and mortality, and returns the model frame (which can be used
-# to prepare the design matrix).
-smoke_fit <- dpaf(
+smoke_fitd <- est_matrix(survival::Surv(F_TIME, DIAB) ~
+                           B_COHORT * F_PERIOD + SEX + SMOKE,
+                         smoke_data,
+                         list(SMOKE = c("Never", "<30/day", ">=30/day")),
+                         covar_model = c("SEX", "SMOKE"))
+smoke_fitm <- est_matrix(survival::Surv(F_TIME, DEATH) ~
+                           B_COHORT * F_PERIOD + SEX + SMOKE,
+                         smoke_data,
+                         list(SMOKE = c("Never", "<30/day", ">=30/day")),
+                         covar_model = c("SEX", "SMOKE"))
+
+smoke_paf <- dpaf_est_paf(smoke_fitd, smoke_fitm, smoke_data)
+
+smoke_summ <- dpaf_summary(
   disease_resp = survival::Surv(F_TIME, DIAB) ~ .,
   death_resp = survival::Surv(F_TIME, DEATH) ~ .,
-  predictors = ~ 0 + B_COHORT*F_PERIOD + SEX + SMOKE,
-  dpaf_data = smoke_data
+  predictors = ~ 0 + B_COHORT * F_PERIOD + SEX + SMOKE,
+  dpaf_data = smoke_data,
+  modifications = list(SMOKE = c("Never", "<30/day", ">=30/day")),
+  covar_model = c("SEX", "SMOKE")
 )
-
-# internally, this calls the summary.dpaf function in R/disease_user.R, but is
-# wrapped around a well-known R function.
-smoke_summ <- summary(smoke_fit,
-                      modlist = list(SMOKE = c("Never", "<30/day", ">=30/day")),
-                      comprehensive = TRUE)
-
-smoke_summ
