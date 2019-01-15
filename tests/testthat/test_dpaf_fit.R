9982511245f41309c593dd18e388b07e517d1088
Ian Powell <i.powell@student.unsw.edu.au>
2019-01-15 16:46:42 +1100

    Update dpaf tests to use new calculation functions

--- tests/testthat/test_dpaf_fit.R
+++ tests/testthat/test_dpaf_fit.R
@@ -4,82 +4,86 @@ context("Disease PAF Morbidity")
 library(rpaf)
 
 test_that("Single-period", {
-  x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
-  cf_d <- c(g1 = -log(0.5), g2 = -log(3))
-  cf_m <- c(g1 = -log(2), g2 = -1)
+    x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
+    cf_d <- c(g1 = -log(0.5), g2 = -log(3))
+    cf_m <- c(g1 = -log(2), g2 = -1)
 
-  id <- gl(3,1)
-  period <- gl(1,3, ordered = TRUE)
+    id <- gl(3,1)
+    period <- gl(1,3, ordered = TRUE)
 
-  hz <- rpaf:::dpaf_hz(x, cbind("disease" = cf_d, "mortality" = cf_m))
-  sv <- rpaf:::dpaf_sv(hz, id, period, dt = c(1))
+    lambda <- rpaf:::dpaf_lambda(x, list("disease" = cf_d, "mortality" = cf_m))
+    S <- rpaf:::dpaf_S(lambda, id, period, dt = c(1))
+    Sp <- rpaf:::dpaf_Sp(S)
+    dSp <- rpaf:::dpaf_delta_Sp(Sp, id, period)
 
-  expect_equal(hz, cbind(disease = c(0.5, 3, 3),
-                         mortality = c(2, exp(1), exp(1))))
-  expect_equal(sv, cbind(disease = c(exp(-0.5), exp(-3), exp(-3)),
+    expect_equal(lambda, list(disease = c(0.5, 3, 3),
+                              mortality = c(2, exp(1), exp(1))))
+    expect_equal(S, list(disease = c(exp(-0.5), exp(-3), exp(-3)),
                          mortality = c(exp(-2), exp(-exp(1)), exp(-exp(1)))))
-  expect_equal(
-    rpaf:::dpaf_i(hz, rpaf:::dpaf_svp(sv), id, period),
-    0.5 / 2.5 * (1 - exp(-2.5)) + 3 / (3 + exp(1)) * (1 - exp(-3-exp(1))) * 2
-  )
+    expect_equal(Sp, c(exp(-2.5), exp(-3 - exp(1)), exp(-3 - exp(1))))
+    expect_equal(dSp, 1 - Sp)
+
+    expect_equal(
+        rpaf:::dpaf_I(lambda, dSp, period),
+        0.5 / 2.5 * (1 - exp(-2.5)) / 3 +
+          3 / (3 + exp(1)) * (1 - exp(-3-exp(1))) * 2 / 3
+    )
 })
 
 test_that("Single-person", {
-  x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
-  cf_d <- c(g1 = -log(0.5), g2 = -log(3))
-  cf_m <- c(g1 = -log(2), g2 = -log(5))
-  cf <- cbind(disease = cf_d, mortality = cf_m)
+    x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
+    cf_d <- c(g1 = -log(0.5), g2 = -log(3))
+    cf_m <- c(g1 = -log(2), g2 = -log(5))
+    cf <- list(disease = cf_d, mortality = cf_m)
 
-  id <- gl(1,3)
-  period <- gl(3,1)
+    id <- gl(1,3)
+    period <- gl(3,1)
 
-  # ss <- dpaf_fit(x, cf_d, cf_m, id, period, 0:3)
+    lambda <- rpaf:::dpaf_lambda(x, cf)
+    S <- rpaf:::dpaf_S(lambda, id, period, c(1,1,1))
+    Sp <- rpaf:::dpaf_Sp(S)
+    dSp <- rpaf:::dpaf_delta_Sp(Sp, id, period)
 
-  hz <- rpaf:::dpaf_hz(x, cf)
-  sv <- rpaf:::dpaf_sv(hz, id, period, c(1,1,1))
-
-  expect_equal(hz, cbind(disease = c(0.5, 3, 3),
-                         mortality = c(2, 5, 5)))
-  expect_equal(sv, cbind(disease = c(exp(-0.5), exp(-3.5), exp(-6.5)),
+    expect_equal(lambda, list(disease = c(0.5, 3, 3),
+                              mortality = c(2, 5, 5)))
+    expect_equal(S, list(disease = c(exp(-0.5), exp(-3.5), exp(-6.5)),
                          mortality = c(exp(-2), exp(-7), exp(-12))))
+    expect_equal(Sp, c(exp(-2.5), exp(-10.5), exp(-18.5)))
 
-  expect_equal(
-    rpaf:::dpaf_i(hz, rpaf:::dpaf_svp(sv), id, period),
-    0.5 / 2.5 * (1 - exp(-2.5)) +
-      3 / 8 * (exp(-2.5) - exp(-10.5)) +
-      3 / 8 * (exp(-10.5) - exp(-18.5))
-  )
+    expect_equal(
+        rpaf:::dpaf_I(lambda, dSp, period),
+        c(0.5 / 2.5 * (1 - exp(-2.5)), 3 / 8 * (exp(-2.5) - exp(-10.5)),
+          3 / 8 * (exp(-10.5) - exp(-18.5)))
+    )
 })
 
 test_that("Uneven period", {
   x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
   cf_d <- c(g1 = -log(0.5), g2 = -log(3))
   cf_m <- c(g1 = -log(2), g2 = -log(5))
-  cf <- cbind(disease = cf_d, mortality = cf_m)
+  cf <- list(disease = cf_d, mortality = cf_m)
 
   id <- gl(1,3)
   period <- gl(3,1)
 
-  # ss <- dpaf_fit(x, cf_d, cf_m, id, period, 0:3)
+  lambda <- rpaf:::dpaf_lambda(x, cf)
+  S <- rpaf:::dpaf_S(lambda, id, period, c(1,1,1/3))
 
-  hz <- rpaf:::dpaf_hz(x, cf)
-  sv <- rpaf:::dpaf_sv(hz, id, period, c(1,1,1/3))
-
-  expect_equal(hz, cbind(disease = c(0.5, 3, 3),
-                         mortality = c(2, 5, 5)))
-  expect_equal(sv, cbind(disease = c(exp(-0.5), exp(-3.5), exp(-3.5 - 3/3)),
-                         mortality = c(exp(-2), exp(-7), exp(-7 - 5/3))))
+  expect_equal(lambda, list(disease = c(0.5, 3, 3),
+                            mortality = c(2, 5, 5)))
+  expect_equal(S, list(disease = c(exp(-0.5), exp(-3.5), exp(-3.5 - 3/3)),
+                       mortality = c(exp(-2), exp(-7), exp(-7 - 5/3))))
 })
 
 test_that("Unsorted periods", {
-  x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
-  cf_d <- c(g1 = -log(0.5), g2 = -log(3))
-  cf_m <- c(g1 = -log(2), g2 = -log(3))
-  cf <- cbind(disease = cf_d, mortality = cf_m)
+    x <- matrix(c(1,0,0, 0,1,1), ncol = 2)
+    cf_d <- c(g1 = -log(0.5), g2 = -log(3))
+    cf_m <- c(g1 = -log(2), g2 = -log(3))
+    cf <- list(disease = cf_d, mortality = cf_m)
 
-  id <- gl(1, 3)
-  period <- factor(c(1,3,2), levels = 1:3, ordered = TRUE)
+    id <- gl(1, 3)
+    period <- factor(c(1,3,2), levels = 1:3, ordered = TRUE)
 
-  hz <- rpaf:::dpaf_hz(x, cf)
-  expect_error(rpaf:::dpaf_sv(hz, id, period, c(1,1,1/3)))
+    lambda <- rpaf:::dpaf_lambda(x, cf)
+    expect_error(rpaf:::dpaf_S(lambda, id, period, c(1,1,1/3)))
 })
--- tests/testthat/test_dpaf_grad.R
+++ tests/testthat/test_dpaf_grad.R
@@ -23,18 +23,18 @@ test_that("hazard and survival gradients", {
   breaks = 0:2
   hz_d <- c(1,2,1,2,4,5,4,5)
   hz_m <- c(2,3,2,3,6,7,6,7)
-  hz <- cbind(disease = hz_d, mortality = hz_m)
+  lambda <- list(disease = hz_d, mortality = hz_m)
 
   sv_d <- exp(-hz_d)
   sv_m <- exp(-hz_m)
-  sv <- cbind(disease = sv_d, mortality = sv_m)
+  S <- list(disease = sv_d, mortality = sv_m)
 
   # grad <- dpaf_grad(z, hz_d, hz_m, sv_d, sv_m, breaks, ID, PERIOD)
 
   # expected values
-  eghz_d <- matrix(c(1,0,0, 0,2,0, 1,0,0, 0,2,0, 4,0,4, 0,5,5, 4,0,4, 0,5,5),
-                   ncol = 3, byrow = TRUE)
-  eghz_m <- matrix(c(2,0,0, 0,3,0, 2,0,0, 0,3,0, 6,0,6, 0,7,7, 6,0,6, 0,7,7),
+  e_glambda_d <- matrix(c(1,0,0, 0,2,0, 1,0,0, 0,2,0, 4,0,4, 0,5,5, 4,0,4, 0,5,5),
+                        ncol = 3, byrow = TRUE)
+  e_glambda_m <- matrix(c(2,0,0, 0,3,0, 2,0,0, 0,3,0, 6,0,6, 0,7,7, 6,0,6, 0,7,7),
                    ncol = 3, byrow = TRUE)
   ecs_ghz_d <- matrix(c(1,0,0, 1,2,0, 1,0,0, 1,2,0,
                         4,0,4, 4,5,9, 4,0,4, 4,5,9),
@@ -42,14 +42,14 @@ test_that("hazard and survival gradients", {
   ecs_ghz_m <- matrix(c(2,0,0, 2,3,0, 2,0,0, 2,3,0,
                         6,0,6, 6,7,13, 6,0,6, 6,7,13),
                       ncol = 3, byrow = TRUE)
-  egsv_d <- -exp(-hz_d) * ecs_ghz_d
-  egsv_m <- -exp(-hz_m) * ecs_ghz_m
+  e_gS_d <- -exp(-hz_d) * ecs_ghz_d
+  e_gS_m <- -exp(-hz_m) * ecs_ghz_m
 
-  ghz <- rpaf:::dpaf_ghz(z, hz)
-  expect_equal(ghz, list(disease = eghz_d, mortality = eghz_m))
+  glambda <- rpaf:::dpaf_grad_lambda(z, lambda)
+  expect_equal(glambda, list(disease = e_glambda_d, mortality = e_glambda_m))
 
-  gsv <- rpaf:::dpaf_gsv(ghz, sv, ID, PERIOD, diff(breaks))
-  expect_equal(gsv, list(disease = egsv_d, mortality = egsv_m))
+  gS <- rpaf:::dpaf_grad_S(glambda, S, ID, PERIOD, diff(breaks))
+  expect_equal(gS, list(disease = e_gS_d, mortality = e_gS_m))
 })
 
 test_that("morbidity gradients", {
@@ -58,43 +58,46 @@ test_that("morbidity gradients", {
   period <- gl(3, 1)
   dt <- c(1,1,1)
 
-  sv <- cbind(disease = c(0.95, 0.85, 0.70), mortality = c(0.99, 0.95, 0.90))
-  hz <- cbind(disease = log(c(1/0.95, 0.95/0.85, 0.85/0.70)),
-              mortality = log(c(1/0.99, 0.99/0.95, 0.95/0.90)))
+  S <- list(disease = c(0.95, 0.85, 0.70), mortality = c(0.99, 0.95, 0.90))
+  lambda <- list(disease = log(c(1/0.95, 0.95/0.85, 0.85/0.70)),
+                 mortality = log(c(1/0.99, 0.99/0.95, 0.95/0.90)))
 
-  stopifnot(all.equal(sv, rpaf:::dpaf_sv(hz, id, period, dt)))
+  stopifnot(all.equal(S, rpaf:::dpaf_S(lambda, id, period, dt)))
 
-  ghz <- list(disease = diag(hz[,1]), mortality = diag(hz[,2]))
+  glambda <- list(disease = diag(lambda$disease),
+                  mortality = diag(lambda$mortality))
 
-  stopifnot(all.equal(ghz, rpaf:::dpaf_ghz(z, hz)))
+  stopifnot(all.equal(glambda, rpaf:::dpaf_grad_lambda(z, lambda)))
 
-  gsv <- list(disease = -sv[,1] %o% hz[,1], mortality = -sv[,2] %o% hz[,2])
-  gsv <- lapply(gsv, function(mat) {mat[upper.tri(mat)] <- 0; mat})
+  gS <- list(disease = -S$disease %o% lambda$disease,
+             mortality = -S$mortality %o% lambda$mortality)
+  gS <- lapply(gS, function(mat) {mat[upper.tri(mat)] <- 0; mat})
 
-  stopifnot(all.equal(gsv, rpaf:::dpaf_gsv(ghz, sv, id, period, dt)))
+  stopifnot(all.equal(gS, rpaf:::dpaf_grad_S(glambda, S, id, period, dt)))
 
   # product of survivals
-  svp <- c(0.9405, 0.8075, 0.6300)
+  Sp <- c(0.9405, 0.8075, 0.6300)
 
-  svp_gsv_d <- (svp * sv[,"disease"]) %o% (-hz[,"disease"])
-  svp_gsv_d[upper.tri(svp_gsv_d)] <- 0
-  svp_gsv_m <- (svp * sv[,"mortality"]) %o% (-hz[,"mortality"])
-  svp_gsv_m[upper.tri(svp_gsv_m)] <- 0
+  Sp_gS_d <- (Sp * S[["disease"]]) %o% (-lambda[["disease"]])
+  Sp_gS_d[upper.tri(Sp_gS_d)] <- 0
+  Sp_gS_m <- (Sp * S[["mortality"]]) %o% (-lambda[["mortality"]])
+  Sp_gS_m[upper.tri(Sp_gS_m)] <- 0
 
-  dsvp <- -diff(c(1, svp))
-  dsvp_gsv_d <- apply(svp_gsv_d, 2, function(col) -diff(c(0, col)))
-  dsvp_gsv_m <- apply(svp_gsv_m, 2, function(col) -diff(c(0, col)))
+  dSp <- -diff(c(1, Sp))
+  dSp_gS_d <- apply(Sp_gS_d, 2, function(col) -diff(c(0, col)))
+  dSp_gS_m <- apply(Sp_gS_m, 2, function(col) -diff(c(0, col)))
+  grad_dSp <- list(disease = dSp_gS_d, mortality = dSp_gS_m)
 
-  dis_prob <- hz[,"disease"] / rowSums(hz)
+  dis_prob <- lambda[["disease"]] / do.call(`+`, lambda)
 
-  gdis_prob_d <- hz[,"mortality"] / rowSums(hz)**2 * ghz$disease
-  gdis_prob_m <- -hz[,"disease"] / rowSums(hz)**2 * ghz$mortality
+  gdis_prob_d <- lambda[["mortality"]] / do.call(`+`, lambda)**2 * glambda$disease
+  gdis_prob_m <- -lambda[["disease"]] / do.call(`+`, lambda)**2 * glambda$mortality
 
   smnd <- list(
-    disease = gdis_prob_d * dsvp + dis_prob * dsvp_gsv_d,
-    mortality = gdis_prob_m * dsvp + dis_prob * dsvp_gsv_m
+    disease = gdis_prob_d * dSp + dis_prob * dSp_gS_d,
+    mortality = gdis_prob_m * dSp + dis_prob * dSp_gS_m
   )
-
-  expect_equal(rpaf:::dpaf_gi(ghz, gsv, hz, sv, id, period),
-               lapply(smnd, colSums))
+  rpaf:::dpaf_grad_I(glambda, grad_dSp, lambda, dSp, period)
+  expect_equal(rpaf:::dpaf_grad_I(glambda, grad_dSp, lambda, dSp, period),
+               smnd)
 })
