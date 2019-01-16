bd5505716457e194efa626ea0e1436a4095311ff
Ian Powell <i.powell@student.unsw.edu.au>
2019-01-16 16:11:02 +1100

    Change call from mpaf_est_matrix to est_matrix

--- R/mortality_paf.R
+++ R/mortality_paf.R
@@ -12,8 +12,8 @@
 #' @export
 mpaf_summary <- function(sr_formula, mpaf_data, modifications, covar_model,
                          prevalence_data, group_vars, level = 0.95, ...) {
-  mpaf_fit <- mpaf_est_matrix(sr_formula, mpaf_data, modifications,
-                              covar_model, level = level, ...)
+  mpaf_fit <- est_matrix(sr_formula, mpaf_data, modifications,
+                         covar_model, level = level, ...)
 
   if (missing(prevalence_data))
     mpaf_all <- mpaf_est_paf(mpaf_fit, mpaf_data, level = level)
